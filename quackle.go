95e5170814a2103746f036711b23f03a6f08498c
H. İbrahim Güngör <igungor@gmail.com>
2017-08-08 03:38:18 +0300

    add a note

--- quackle.go
+++ quackle.go
@@ -1,3 +1,6 @@
+// XXX: Note that `go build` fails if the swig interface filename is the same
+// as this filename. So we can't rename this file to quackle.go.
+
 package quackle
 
 // #cgo CXXFLAGS: -I${SRCDIR}/_include
